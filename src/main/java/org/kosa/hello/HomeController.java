ebc3b53f3cfe1f194c64ac53ce2a13ef987db11a
Sibaru1217 <leehakbin1217@gmail.com>
2025-06-11 11:23:54 +0900

    first commit

---
+++ src/main/java/org/kosa/hello/HomeController.java
@@ -0,0 +1,20 @@
+package org.kosa.hello;
+
+import jakarta.servlet.ServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    // SLF4J Logger 설정
+    private static final Logger log = LoggerFactory.getLogger(HomeController.class);
+
+    @GetMapping("/")
+    public String index(ServletRequest request) {
+        log.info("server port = {}", request.getServerPort());
+        return "Hello World... " + request.getServerPort();
+    }
+}
